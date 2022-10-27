92677df2075d9cba33b8d5239198dc645d84568c
Nyzio <39226151+Nyzio2197@users.noreply.github.com>
2022-10-26 21:26:50 -0400

    Build production client

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c9b93bb83d80f1faefc468b1eb42c0d.js"
+  "/precache-manifest.68aef73f9126aa1d86412866c248695f.js"
 );
 
 self.addEventListener('message', (event) => {
